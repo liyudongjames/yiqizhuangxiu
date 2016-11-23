abc81baa8e883c9728a51e825cdcec17aca9fbd9
liyudongjames <1012956221@qq.com>
2016-11-23 11:06:33 +0800

    first version

---
+++ app/src/main/java/com/bwf/yiqizhuang/MyApplication.java
@@ -0,0 +1,15 @@
+package com.bwf.yiqizhuang;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/11/23.
+ */
+
+public class MyApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
